a3936e6170cff347bce19ee317d3a6ee81c2f624
PraisEfe <103075978+PraisEfe@users.noreply.github.com>
2026-01-07 18:18:21 +0000

    designed the download section adn footer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,17 @@
 import Blob from "../assets/blob";
 import doodle from "../assets/doodle.svg";
+import Download from "../Components/Download.jsx";
+import Footer from "../Components/Footer.jsx";
+import PageNotFound from "./PageNotFound.jsx";
+
 export default function Home() {
   return (
     <div
       className="h-screen w-screen"
       // style={{ backgroundImage: `url(${doodle})` }}
     >
-      sometibg
+      <Download />
+      <Footer />
     </div>
   );
 }
